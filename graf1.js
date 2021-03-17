f73526049494ae61183618dd83b6889d2e636f30
nelakrawiecova <nela.krawiecova@gmail.com>
2021-03-17 16:45:00 +0100

    sirka_grafu

--- graf1.js
+++ graf1.js
@@ -5,7 +5,7 @@
 
   function onChartLoad(e) {
     const plotBack = document.getElementById(e.renderTo.id).getElementsByClassName('highcharts-plot-background')[0];
-    const shouldBeHeight = (plotBack.width.baseVal.value / 4) * 3.2;
+    const shouldBeHeight = (plotBack.width.baseVal.value / 2) * 2;
     const heightDiff = shouldBeHeight - plotBack.height.baseVal.value;
     if (heightDiff > 0) {
       document.getElementById(e.renderTo.id).style.height = `${e.chartHeight + heightDiff}px`;
