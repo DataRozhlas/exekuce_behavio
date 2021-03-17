905bead2c5237630657df3d41a3456db4aabd386
nelakrawiecova <nela.krawiecova@gmail.com>
2021-03-17 13:30:14 +0100

    zmena4

--- graf4.js
+++ graf4.js
@@ -1,90 +1,105 @@
 // Nastaveni max sirky grafu
-const chartWidthNehody = document.getElementById('vis-column-chart-simple').offsetWidth > 600
-  ? 600
-  : document.getElementById('vis-column-chart-simple').offsetWidth;
+//const chartWidthNehody = document.getElementById('vis-column-chart-simple').offsetWidth > 600
+ // ? 600
+  //: document.getElementById('vis-column-chart-simple').offsetWidth;
 
-function onChartLoad(e) {
-  const plotBack = document.getElementById(e.renderTo.id).getElementsByClassName('highcharts-plot-background')[0];
-  const shouldBeHeight = (plotBack.width.baseVal.value / 4) * 3;
-  const heightDiff = shouldBeHeight - plotBack.height.baseVal.value;
-  if (heightDiff > 0) {
-    document.getElementById(e.renderTo.id).style.height = `${e.chartHeight + heightDiff}px`;
-    e.reflow();
+  function onChartLoad(e) {
+    const plotBack = document.getElementById(e.renderTo.id).getElementsByClassName('highcharts-plot-background')[0];
+    const shouldBeHeight = (plotBack.width.baseVal.value / 4) * 3.2;
+    const heightDiff = shouldBeHeight - plotBack.height.baseVal.value;
+    if (heightDiff > 0) {
+      document.getElementById(e.renderTo.id).style.height = `${e.chartHeight + heightDiff}px`;
+      e.reflow();
+    }
   }
-}
-
-Highcharts.chart('graf4', {
-  chart: {
-    type: 'column',
-    width: chartWidthNehody,
-    events: {
-      load() {
-        onChartLoad(this);
+  
+  Highcharts.chart('graf4', {
+      chart: {
+        type: 'bar',
+        //width: chartWidthNehody,
+        events: {
+          load() {
+            onChartLoad(this);
+          },
+        },
+    
       },
-    },
-  },
-  title: {
-    text: 'Column Chart Single',
-    useHTML: true,
-  },
-  subtitle: {
-    text: 'Celkový počet nepovolených průjezdů byl v&nbsp;loňském prvním pololetí vyšší než letos',
-    // + '<br><span style="color: #fff">.</span>',
-    useHTML: true,
-  },
-  xAxis: {
-    categories: ['<span class="eighteen">2018</span>',
-      '<span class="nineteen">2019</span>',
-      '<span class="twenty">2020</span>'],
-    labels: {
-      enabled: true,
-    },
-  },
-  yAxis: {
-    title: {
-      text: 'nepovolené jízdy za návěstidla',
-    },
-    // labels: {
-    //   formatter: function() {
-    //     if (this.isLast) {
-    //       return this.value + '<br>' +
-    //                   '<span class="light-gray-text">jízd za</span>' + '<br>' +
-    //                   '<span class="light-gray-text">návěstidla</span>'
-    //     } else {
-    //       return this.value
-    //     }
-    //   }
-    // }
-  },
-  legend: {
-    enabled: false,
-  },
-  tooltip: {
-    valueSuffix: ' jízd',
-    // shared: true
-  },
-  exporting: {
-    enabled: false,
-  },
-  credits: {
-    href: 'http://www.dicr.cz/statistiky-mimoradnych-udalosti',
-    text: 'Zdroj: Drážní inspekce',
-  },
-  plotOptions: {
-    column: {
-      dataLabels: {
-        enabled: true,
+      title: {
+        text: 'Kterým službám má stát pomoci?',
+        useHTML: true,
       },
-      // enableMouseTracking: false, // odkomentuj, pokud nechces tooltip
-      pointPadding: 0,
-    },
-  },
-  series: [
-    {
-      name: 'Celkový počet mimořádných událostí',
-      data: [{ y: 85, color: colors['2018'] },
-        { y: 97, color: colors['2019'] },
-        { y: 88, color: colors['2020'] }],
-    },
-  ],
-});
+      subtitle: {
+        text: '87 procent odborníků považuje za nutné podpořit sociálně aktivizační služby pro rodiny s dětmi',
+        // + '<br><span style="color: #fff">.</span>',
+        useHTML: true,
+      },
+      xAxis: {
+        categories: ['Střediska výchovné péče',
+        'Ubytovny',
+        'Raná péče',
+        'Psychiatr',
+        'Adiktologické služby',
+        'Asistenční služby',
+        'Manželské poradny',
+        'Psycholog',
+        'Azylové domy',
+        'Sociálně aktivizační služby'],
+        labels: {
+          enabled: true,
+        },
+      },
+      yAxis: {
+        title: {
+          text: '',
+        },
+        // labels: {
+        //   formatter: function() {
+        //     if (this.isLast) {
+        //       return this.value + '<br>' +
+        //                   '<span class="light-gray-text">jízd za</span>' + '<br>' +
+        //                   '<span class="light-gray-text">návěstidla</span>'
+        //     } else {
+        //       return this.value
+        //     }
+        //   }
+        // }
+      },
+      legend: {
+        enabled: false,
+      },
+      tooltip: {
+        valueSuffix: 'procent dotázaných souhlasí',
+        shared: true
+      },
+      exporting: {
+        enabled: false,
+      },
+      credits: {
+        href: 'https://nadacesirius.cz/soubory/aktuality/Prognoza-vyvoje-situace-pro-rodiny-s-detmi-a-podpurne-sluzby.pdf',
+        text: 'Zdroj: Median a Nadace Sirius',
+      },
+      plotOptions: {
+        column: {
+          dataLabels: {
+            enabled: true,
+          },
+          // enableMouseTracking: false, // odkomentuj, pokud nechces tooltip
+          pointPadding: 1,
+        },
+      },
+      series: [
+        {
+          name: 'stát by měl podpořit',
+          data: [{ y: 17, color: colors['2020'] },
+            { y: 25, color: colors['2020'] },
+            { y: 33, color: colors['2020'] },
+            { y: 36, color: colors['2019'] },
+            { y: 46, color: colors['2020'] },
+            { y: 41, color: colors['2020'] },
+            { y: 44, color: colors['2020'] },
+            { y: 62, color: colors['2019'] },
+            { y: 70, color: colors['2020'] },
+            { y: 86, color: colors['2020'] }],
+        },
+      ],
+    });
